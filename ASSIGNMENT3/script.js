6adc51ff45ce1b1c400ef7c50f268e3a502b85ab
Shruti-Pashine-BBI <141905762+Shruti-Pashine-BBI@users.noreply.github.com>
2023-08-11 11:52:33 +0530

    Update script.js

--- ASSIGNMENT3/script.js
+++ ASSIGNMENT3/script.js
@@ -1,10 +1,32 @@
-const rectangles = document.querySelectorAll('.rectangle');
 
-rectangles.forEach(rectangle => {
-  rectangle.addEventListener('click', () => {
-    rectangles.forEach(r => {
-      r.classList.remove('active');
+
+let rect=document.getElementById("Rectangle1")
+let rect1=document.getElementById("Rectangle2")
+let rect2=document.getElementById("Rectangle3")
+
+    
+    rect.addEventListener("click",function()
+    {
+        if(rect.style.backgroundColor= 'black'){
+            rect1.style.backgroundColor= 'white';
+            rect2.style.backgroundColor= 'white';
+        }
     });
-    rectangle.classList.add('active');
-  });
-});
+    rect1.addEventListener("click",function()
+    {
+        if(rect1.style.backgroundColor= 'black'){
+            rect.style.backgroundColor= 'white';
+            rect2.style.backgroundColor= 'white';
+        }
+    });
+    rect2.addEventListener("click",function()
+    {
+        if(rect2.style.backgroundColor= 'black'){
+            rect.style.backgroundColor= 'white';
+            rect1.style.backgroundColor= 'white';
+           
+        }
+    });
+
+
+
